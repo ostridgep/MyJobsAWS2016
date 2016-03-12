b439b273179f768e085ab60a160be4c5a070f548
Paul Ostridge <Paul@ostridge.com>
2016-03-12 17:14:45 +0000

    x

--- MyJobsAWS2016/WebContent/myresources/js/documents.js
+++ MyJobsAWS2016/WebContent/myresources/js/documents.js
@@ -327,7 +327,7 @@ function buildDocumentTables(){
 //get photo and store locally
 function getPhoto() {
     // Take picture using device camera and retrieve image as base64-encoded string
-	alert("about to take photo"+cordova.file.dataDirectory+":"+cordova.file.applicationStorageDirectory+":"+cordova.file.cacheDirectory)
+	alert("about to take photo"+cordova.file.dataDirectory)
 	
     navigator.camera.getPicture(onGetPhotoDataSuccess, onGetPhotoDataFail, { quality: 50 });
 }
@@ -341,7 +341,7 @@ function onGetPhotoDataSuccess(imageData) {
                        + (currentdate.getMinutes()).toString()
                        + (currentdate.getSeconds()).toString();
     alert(imageData)
-    moveFile(imageData, cordova.file.dataDirectory+"files/MyJobs/Private/Photos/")
+    moveFile(imageData, cordova.file.dataDirectory+"MyJobs/Private/Photos/")
     //moveFile(imageData,cordova.file.dataDirectory + "MyJobs/Global/")
 }
 
