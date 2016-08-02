885e917dfdf7b6ad875583b0c9c23e792561c4c1
ostridgep <paul@ostridge.com>
2016-08-02 23:26:36 +0100

    xx

--- MyJobsAWS2016/WebContent/myresources/js/documents.js
+++ MyJobsAWS2016/WebContent/myresources/js/documents.js
@@ -1600,9 +1600,9 @@ function downloadAsset(fileName,dir) {
 }
 function downloadAllAsset(fileName,dir) {
     var fileTransfer = new FileTransfer();
-    fileTransfer.download(localStorage.getItem("DOCSERVER")+dir + fileName, cordova.file.externalApplicationStorageDirectory + dir +fileName,
+    fileTransfer.download(localStorage.getItem("DOCSERVER")+dir+"/" + escape(fileName), cordova.file.externalApplicationStorageDirectory + dir + "/"+escape(fileName),
 		function (entry) {
-    	console.log("Downloading"+entry.fullPath)
+    	
 		   
 		},
 		function (error) {
