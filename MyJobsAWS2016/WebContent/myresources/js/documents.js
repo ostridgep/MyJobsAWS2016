8a1fd15f0cb9b08ac098ddc3f3952a75a0ef21a2
ostridgep <paul@ostridge.com>
2016-08-10 15:59:29 +0100

    xx

--- MyJobsAWS2016/WebContent/myresources/js/documents.js
+++ MyJobsAWS2016/WebContent/myresources/js/documents.js
@@ -294,7 +294,6 @@ function getBase64FromImageUrl(imageUri,id,name) {
 function createBase64XML(base64,fn,id,name){
 	xx=fn.split(".")
 	dt=getFileUploadDT()
-	alert('<filename>'+CurrentOrderNo+CurrentOpNo+'-'+name+'.'+xx[1]+'</filename>')
 	var xmlstring =  '<uploadRequest userName="'+localStorage.getItem('MobileUser')+'" userRole="Y008 Desc" userMyalmScenario="Y008" machineName="'+localStorage.getItem('MobileUser')+'">'+
 					  '<jobMetadata>'+
 					  '  <order>'+CurrentOrderNo+'</order>'+
@@ -327,7 +326,6 @@ function createBase64XML(base64,fn,id,name){
 function createBase64FormXML(base64,fn,id,name){
 	dt=getFileUploadDT()
 	uploadfn=CurrentOrderNo+'0010-'+dt+'-'+fn
-	alert( '  <filename>'+CurrentOrderNo+CurrentOpNo+'-'+name+'.html</filename>');
 	var xmlstring =  '<uploadRequest userName="'+localStorage.getItem('MobileUser')+'" userRole="Y008 Desc" userMyalmScenario="Y008" machineName="'+localStorage.getItem('MobileUser')+'">'+
 					  '<jobMetadata>'+
 					  '  <order>'+CurrentOrderNo+'</order>'+
@@ -1497,7 +1495,7 @@ function RequestLLFile(params)
 }
 function sendPhotoToServer(id,fname,content){
 	xmlname=getFileUploadName()+".xml"
-	alert("Photo"+xmlname)
+	
 	updatePhotoState(id,"Sending")
 	buildJobDocsTable()
 	var jqxhr = $.post( localStorage.getItem("DOCSERVER")+'PhotoUpload.php',
@@ -1528,7 +1526,7 @@ function sendPhotoToServer(id,fname,content){
 function sendDocToServer(id,fname,content){
 	
 	xmlname=getFileUploadName()+".xml"
-	alert("Doc"+xmlname)
+	
 	updateDocumentState(id,"Sending")
 	buildJobDocsTable()
 	var jqxhr = $.post( localStorage.getItem("DOCSERVER")+'PhotoUpload.php',
