15058acb4cbd7fa389f70fa9929627a4a6009b4b
ostridgep <paul@ostridge.com>
2016-08-10 13:19:14 +0100

    xx

--- MyJobsAWS2016/WebContent/myresources/js/MyJobsDB.js
+++ MyJobsAWS2016/WebContent/myresources/js/MyJobsDB.js
@@ -3547,7 +3547,7 @@ if(rowsArray.length>0){
 	
 	formHTML=window.btoa(HTMLFormStart+y+HTMLFormEnd)
 	
-	createBase64FormXML(formHTML,rowsArray[0].formdesc+".html",id,formdesc)	
+	createBase64FormXML(formHTML,rowsArray[0].formdesc+".html",id,rowsArray[0].formdesc)	
 
 }
 
